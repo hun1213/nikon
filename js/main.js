516c627ffcffa3cb9517263e16ed1a0a1b5dd9f3
Hooncheol <hun2067@gmail.com>
2022-05-02 16:08:23 +0900

    1.9

--- js/main.js
+++ js/main.js
@@ -39,6 +39,7 @@ $(function () {
   $(".sub-wrapper").mouseleave(function () {
     $(this).find(".sub").stop().slideUp(300);
   });
+
   //search 시작
   $(".search-icon").on("click", function () {
     $(".search").stop().slideDown(300);
@@ -60,23 +61,23 @@ $(function () {
   $(".video2_1").mouseenter(function () {
     let video2_1On = $(".video2_1 a img");
 
-    video2_1On.attr("src", "/img/videoSub1_on.png");
+    video2_1On.attr("src", "img/videoSub1_on.png");
   });
   $(".video2_2").mouseenter(function () {
     let video2_2On = $(".video2_2 a img");
 
-    video2_2On.attr("src", "/img/videoSub2_on.png");
+    video2_2On.attr("src", "img/videoSub2_on.png");
   });
 
   $(".video2_1").mouseleave(function () {
     let video2_1Off = $(".video2_1 a img");
 
-    video2_1Off.attr("src", "/img/videoSub1_off.png");
+    video2_1Off.attr("src", "img/videoSub1_off.png");
   });
   $(".video2_2").mouseleave(function () {
     let video2_2Off = $(".video2_2 a img");
 
-    video2_2Off.attr("src", "/img/videoSub2_off.png");
+    video2_2Off.attr("src", "img/videoSub2_off.png");
   });
   //video2 끝
 });
