8938db5476139ab11e0dadecd13ffbe043f89d77
Hooncheol <hun2067@gmail.com>
2022-05-04 23:41:17 +0900

    2.3

---
+++ js/e-shop.js
@@ -0,0 +1,19 @@
+$(function () {
+  $(document).ready(function () {
+    $(".product-box").slick({
+      infinite: false,
+      slidesToShow: 5,
+      arrows: true,
+      slidesToScroll: 1,
+    });
+  });
+
+  $(document).ready(function () {
+    $(".timesale-box").slick({
+      infinite: false,
+      slidesToShow: 5,
+      arrows: true,
+      slidesToScroll: 1,
+    });
+  });
+});
--- js/main.js
+++ js/main.js
@@ -61,23 +61,23 @@ $(function () {
   $(".video2_1").mouseenter(function () {
     let video2_1On = $(".video2_1 a img");
 
-    video2_1On.attr("src", "img/videoSub1_on.png");
+    video2_1On.attr("src", "img/main/videoSub1_on.png");
   });
   $(".video2_2").mouseenter(function () {
     let video2_2On = $(".video2_2 a img");
 
-    video2_2On.attr("src", "img/videoSub2_on.png");
+    video2_2On.attr("src", "img/main/videoSub2_on.png");
   });
 
   $(".video2_1").mouseleave(function () {
     let video2_1Off = $(".video2_1 a img");
 
-    video2_1Off.attr("src", "img/videoSub1_off.png");
+    video2_1Off.attr("src", "img/main/videoSub1_off.png");
   });
   $(".video2_2").mouseleave(function () {
     let video2_2Off = $(".video2_2 a img");
 
-    video2_2Off.attr("src", "img/videoSub2_off.png");
+    video2_2Off.attr("src", "img/main/videoSub2_off.png");
   });
   //video2 끝
 });
