72f5ac238f294fbf3ece9ac370222b19afafbbf7
Hooncheol <hun2067@gmail.com>
2022-05-15 14:49:23 +0900

    3.7 add cs-center tablet version

--- js/e-shop.js
+++ js/e-shop.js
@@ -16,6 +16,42 @@ $(function () {
     $(".search").stop().slideUp(300);
     $(".modal").stop().hide();
   };
+
+  //햄버거 메뉴
+  toggleMobileMenu = () => {
+    if ($(".hamburger-icon-tm").hasClass("active-1") === true) {
+      closeMobileMenu();
+    } else {
+      openMobileMenu();
+    }
+  };
+
+  openMobileMenu = () => {
+    $(".hamburger-icon-tm").addClass("active-1");
+    $(".gnb-menu-wrapper-tm").stop().slideDown(300);
+  };
+  closeMobileMenu = () => {
+    $(".hamburger-icon-tm").removeClass("active-1");
+    $(".gnb-menu-wrapper-tm").stop().slideUp(300);
+    $(".gnb-sub-tm").stop().hide();
+  };
+
+  $(window).resize(function () {
+    const windowWidth = $(window).width();
+    if (windowWidth >= 1080) {
+      $(".gnb-menu-wrapper-tm").css("display", "none");
+      $(".hamburger-icon-tm").removeClass("active-1");
+    }
+  });
+
+  $(".hamburger-icon-tm").click(function () {
+    toggleMobileMenu();
+  });
+
+  $(".logo, .gnb-logo-tm").click(function () {
+    closeMobileMenu();
+    closeSearch();
+  });
   //search 끝
 
   $(document).ready(function () {
@@ -30,6 +66,13 @@ $(function () {
           settings: {
             slidesToShow: 4,
             slidesToScroll: 2,
+          },
+        },
+        {
+          breakpoint: 722,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
           },
         },
       ],
@@ -50,7 +93,39 @@ $(function () {
             slidesToScroll: 2,
           },
         },
+        {
+          breakpoint: 722,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+          },
+        },
       ],
     });
+  });
+
+  //장바구니
+
+  $(".put-in-the-cart img").on("click", function () {
+    const HEARTOFF = "img/Eshop/icons/heart.svg";
+    const HEARTON = "img/Eshop/icons/heart_on.svg";
+    let heartSrc = $(this).attr("src") === HEARTOFF ? HEARTON : HEARTOFF;
+
+    $(this).attr("src", heartSrc);
+
+    const HIDECOUNT = $(".count-reserved-items").hide();
+    showCount = () => {
+      $(".count-reserved-items").show();
+      $(".count-reserved-items").text(CountReservedItems);
+    };
+    let CountReservedItems = $(
+      "img[src$='img/Eshop/icons/heart_on.svg'"
+    ).length;
+
+    if (CountReservedItems >= 1) {
+      showCount();
+    } else {
+      HIDECOUNT;
+    }
   });
 });
---
+++ js/hi.js
@@ -0,0 +1,10 @@
+const HEARTICON = document.getElementsByClassName("put-in-the-cart");
+const HEARTIMAGE = HEARTICON.children;
+const HEARTOFF = "img/Eshop/icons/heart.svg";
+const HEARTON = "img/Eshop/icons/heart_on.svg";
+
+console.log(HEARTIMAGE);
+// HEARTICON.onclick = () => {
+//   console.log(this);
+
+// };
